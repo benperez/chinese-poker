b44e3ea044bd3bf0428646168ebd79b568d840f7
Ben Perez <ben.prez@gmail.com>
2015-08-18 10:26:36 -0700

    extract rank-based hand types

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+import Data.List (groupBy, sort)
 
 data Suit = Diamonds
           | Clubs
@@ -40,3 +41,15 @@ data Hand = Hand Card Card Card Card Card deriving (Eq, Show)
 --TODO - this is where hand comparison logic goes
 instance Ord Hand where
   (Hand a _ _ _ _) `compare` (Hand b _ _ _ _) = a `compare` b
+
+extractFrequencyHandTypes :: Hand -> HandType
+extractFrequencyHandTypes (Hand c1 c2 c3 c4 c5) = case rankFreqs of
+  [4, 1]       -> Quads
+  [3, 2]       -> FullHouse
+  [3, 1, 1]    -> Trips
+  [2, 2, 1]    -> TwoPair
+  [2, 1, 1, 1] -> Pair
+  _            -> HighCard
+  where
+    ranksEqual (Card rank1 _) (Card rank2 _) = rank1 == rank2
+    rankFreqs = map length . groupBy ranksEqual . sort $ [c1, c2, c3, c4, c5]
