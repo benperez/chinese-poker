8c6532c41d977db82c64c4291496b585f3f4ffd5
Benjamin Perez <ben.prez@gmail.com>
2016-01-31 23:29:57 -0800

    first pass at tie-breaking hands

--- src/Hand.hs
+++ src/Hand.hs
@@ -1,85 +1,88 @@
 module Hand (Suit(..), Rank(..), Card(..), Hand(Hand), validateHands) where
 
-import Data.List (group, sort)
-import Data.List.Unique (repeated)
+import Data.List (group, groupBy, sort, sortOn)
 
 
-data Suit = Diamonds
-          | Clubs
-          | Hearts
-          | Spades deriving (Eq, Enum, Ord, Show)
+data Suit = Diamonds | Clubs | Hearts | Spades
+  deriving (Eq, Enum, Ord, Show)
 
-data Rank = Two
-          | Three
-          | Four
-          | Five
-          | Six
-          | Seven
-          | Eight
-          | Nine
-          | Ten
-          | Jack
-          | Queen
-          | King
-          | Ace
-          deriving (Eq, Enum, Ord, Show)
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+  deriving (Eq, Enum, Ord, Show)
+
+type Card = (Rank, Suit)
+
+data Hand = Hand Card Card Card Card Card
+  deriving (Show)
 
 data HandType = HighCard
               | Pair
               | TwoPair
               | Trips
-              --a wheel straight is a 5 high straight
               | WheelStraight
               | Straight
               | Flush
               | FullHouse
               | Quads
-              --a wheel straight flush is a 5 high straight flush
               | WheelStraightFlush
               | StraightFlush
-              deriving (Eq, Enum, Ord, Show)
-
-type Card = (Rank, Suit)
-
-data Hand = Hand Card Card Card Card Card deriving (Eq, Show)
+              deriving (Eq, Ord, Show)
 
 getCards :: Hand -> [Card]
 getCards (Hand a1 a2 a3 a4 a5) = [a1, a2, a3, a4, a5]
 
-getSuits :: Hand -> [Suit]
-getSuits = map snd . getCards
+getCardsSortedByRank :: Hand -> [Card]
+getCardsSortedByRank = sortOn fst . getCards
 
-getSortedRanks :: Hand -> [Rank]
-getSortedRanks = sort . map fst . getCards
+repeated :: Ord a => [a] -> [a]
+repeated = map head . filter ((>1) . length) . group . sort
 
 validateHands :: [Hand] -> Bool
 validateHands = (== 0) . length . repeated . concatMap getCards
 
+isStraight :: [Rank] -> Bool
+isStraight ranks = and $ zipWith (\a b -> b == succ a) ranks (drop 1 ranks)
+
+isWheelStraight :: [Rank] -> Bool
+isWheelStraight = (== [Two, Three, Four, Five, Ace])
+
+isFlush :: [Suit] -> Bool
+isFlush suits = and $ map (== head (suits)) (tail (suits))
+
+extractHandType :: Hand -> (HandType, (Rank, Rank, Rank, Rank, Rank))
+extractHandType h = case increasingRankFrequencies of
+  [(1,[a]),   (4,[b,c,d,e])]                        -> (Quads,     (e, d, c, b, a))
+  [(2,[a,b]), (3,[c,d,e])]                          -> (FullHouse, (e, d, c, b, a))
+  [(1,[a]),   (1,[b]),   (3,[c,d,e])]               -> (Trips,     (e, d, c, b, a))
+  [(1,[a]),   (2,[b,c]), (2,[d,e])]                 -> (TwoPair,   (e, d, c, b, a))
+  [(1,[a]),   (1,[b]),   (1,[c]), (2,[d,e])]        -> (Pair,      (e, d, c, b, a))
+  [(1,[a]),   (1,[b]),   (1,[c]), (1,[d]), (1,[e])] -> case (isStraight rs, isWheelStraight rs, isFlush ss) of
+    (True,  False, True)  -> (StraightFlush,      (e, d, c, b, a))
+    (False, True,  True)  -> (WheelStraightFlush, (e, d, c, b, a))
+    (False, False, True)  -> (Flush,              (e, d, c, b, a))
+    (True,  False, False) -> (Straight,           (e, d, c, b, a))
+    (False, True,  False) -> (WheelStraight,      (e, d, c, b, a))
+    (False, False, False) -> (HighCard,           (e, d, c, b, a))
+  where
+    sortedCards = getCardsSortedByRank h
+    rs = map fst sortedCards
+    ss = map snd sortedCards
+    ranksEqual a b = (fst a) == (fst b)
+    increasingRankFrequencies = sortOn fst .
+                                map (\g -> (length g, g)) .
+                                group $ rs
+
 instance Ord Hand where
   a `compare` b
     | handTypeA /= handTypeB = handTypeA `compare` handTypeB
-    | otherwise              = (getSortedRanks a) `compare` (getSortedRanks b)
+    | otherwise              = ranksA    `compare` ranksB
     where
-      handTypeA = extractHandType a
-      handTypeB = extractHandType b
+      (handTypeA, ranksA) = extractHandType a
+      (handTypeB, ranksB) = extractHandType b
 
-extractHandType :: Hand -> HandType
-extractHandType h = case increasingRankFrequencies of
-  [1, 4]          -> Quads
-  [2, 3]          -> FullHouse
-  [1, 1, 3]       -> Trips
-  [1, 2, 2]       -> TwoPair
-  [1, 1, 1, 2]    -> Pair
-  [1, 1, 1, 1, 1] -> case (isStraight, isWheelStraight, isFlush) of
-    (True,  False, True)  -> StraightFlush
-    (False, True,  True)  -> WheelStraightFlush
-    (False, False, True)  -> Flush
-    (True,  False, False) -> Straight
-    (False, True,  False) -> WheelStraight
-    (False, False, False) -> HighCard
-  where
-    ranks = getSortedRanks h
-    increasingRankFrequencies = sort $ map length $ group ranks
-    isStraight = and $ zipWith (\a b -> b == succ a) ranks (drop 1 ranks)
-    isWheelStraight = ranks == [Two, Three, Four, Five, Ace]
-    isFlush = and $ map (== head (getSuits h)) (tail (getSuits h))
+instance Eq Hand where
+  a == b
+    | handTypeA /= handTypeB = False
+    | otherwise              = ranksA == ranksB
+    where
+      (handTypeA, ranksA) = extractHandType a
+      (handTypeB, ranksB) = extractHandType b
--- test/Hand/HandSpec.hs
+++ test/Hand/HandSpec.hs
@@ -1,6 +1,6 @@
 module Hand.HandSpec (main, spec) where
 
-import Data.List (sort)
+import Data.List (group, sort)
 
 import Test.Hspec
 
@@ -70,7 +70,7 @@ invalidHand   = Hand (Eight, Clubs)
 spec :: Spec
 spec = do
   describe "hand implementation of Ord" $ do
-    it "correctly sorts a deck of hands" $
+    it "sorts a deck of hands" $
       sort unsortedHandArray `shouldBe` [ highCard
                                         , pair
                                         , twoPair
@@ -81,17 +81,30 @@ spec = do
                                         , fourOfAKind
                                         , straightFlush
                                         ]
+    it "breaks ties when the hands have the same type" $
+      let hc1    = Hand (Ace, Diamonds) (Seven, Hearts) (Five, Clubs) (Three, Diamonds) (Two, Spades)
+          hc2    = Hand (King, Diamonds) (Seven, Hearts) (Five, Clubs) (Three, Diamonds) (Two, Spades)
+          two1    = Hand (King, Hearts) (King, Spades) (Jack, Clubs) (Jack, Diamonds) (Nine, Diamonds)
+          two2    = Hand (King, Hearts) (King, Spades) (Ten, Clubs) (Ten, Diamonds) (Nine, Diamonds)
+          f1     = Hand (King, Spades) (Jack, Spades) (Nine, Spades) (Seven, Spades) (Three, Spades)
+          f2     = Hand (King, Hearts) (Jack, Hearts) (Nine, Hearts) (Seven, Hearts) (Three, Hearts)
+      in group (sort [hc1, hc2, two1, two2, f1, f2]) `shouldBe` [ [hc2]
+                                                                , [hc1]
+                                                                , [two2]
+                                                                , [two1]
+                                                                , [f1, f2]
+                                                                ]
   describe "hand validation" $ do
-    it "correctly identifies a valid hand" $
+    it "validates a correct hand" $
       validateHands [flush] `shouldBe` True
 
-    it "correctly identifies an invalid hand" $
+    it "invalidates a hand with a repeated card" $
       validateHands [invalidHand] `shouldBe` False
 
-    it "correctly identifies two valid hands" $
+    it "validates two hands with different cards" $
       validateHands [highCard, twoPair] `shouldBe` True
 
-    it "correctly identifies two invalid hands" $
+    it "invalidates two hands with the same card" $
       validateHands [highCard, pair] `shouldBe` False
 
 main :: IO ()
